5da29e5e5e06f4de31da0bf81e9e1fd7447a05ce
kevin <kekekevin@gmail.com>
2014-08-15 16:55:52 +0000

    challenge 3

---
+++ lib/set1/challenge3.rb
@@ -0,0 +1,21 @@
+class Challenge3
+  
+  def xor(buffer, char)
+    xor = buffer.each_char.each_slice(2).collect { |x| x.join.hex ^ char.ord }
+  end
+  
+  def score(buffer, char)
+    message = xor(buffer, char)
+    score = message.select { |x| x.between?( "A".ord, "Z".ord) || x.between?( "a".ord, "z".ord) }.count
+    { :score => score, :message => message.pack("c*") , :char => char.chr }
+  end
+  
+  def decode
+    scores = (1..200).collect do |char|
+      self.score("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", char)
+    end
+    
+    puts scores.sort { |x,y| x[:score] <=> y[:score] }.reverse[0..5]
+  end  
+    
+end
