eccf5deee9baa3f17cc1f4f3e85c32e53519f8df
kevin <kekekevin@gmail.com>
2014-08-19 19:11:59 +0000

    challenge 6, find candidate keysizes

--- lib/set1/challenge6.rb
+++ lib/set1/challenge6.rb
@@ -1,4 +1,10 @@
+require "base64"
+
 class Challenge6
+  
+  def initialize
+    @cypher_text = Base64.decode64(IO.readlines(File.dirname(__FILE__) +"/challenge6.txt").join)
+  end
   
   def hamming_distance(string1, string2)
     
@@ -6,6 +12,18 @@ class Challenge6
     bits = byte_xor.pack("c*").unpack("b*")[0]
     bits.count "1"
     
+  end
+  
+  def find_keysize()
+    distances = []
+    
+    range = 2..40
+    range.collect do |keysize|
+      distance = hamming_distance(@cypher_text[0..keysize], @cypher_text[keysize..(keysize*2)])
+      distances << { :distance => distance, :keysize => keysize }
+    end
+    
+    distances.sort_by { |distance| distance[:distance] }[0..2]
   end
   
 end
